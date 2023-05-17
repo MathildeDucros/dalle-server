885f7f3c07f0b349245af25d1017b053446bd445
Mathilde <ducros.mathilde@gmail.com>
2023-05-17 22:25:18 +0200

    last commit

--- urls.js
+++ urls.js
@@ -1,4 +1,2 @@
-const BACKEND_URL = "https://dalle-back.vercel.app/";
-const FRONTEND_URL = "https://dalle-front.vercel.app/";
-
-module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
+BACKEND_URL = "https://dalle-backend-beta.vercel.app/";
+FRONTEND_URL = "https://dalle-front.vercel.app/";
